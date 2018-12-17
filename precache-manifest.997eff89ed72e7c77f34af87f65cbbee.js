8a1b119cba7422d6d972154c4fd7448c3782d8d1
gabiprds93 <gabiprds.93@gmail.com>
2018-12-17 10:52:24 -0500

    Updates

---
+++ precache-manifest.997eff89ed72e7c77f34af87f65cbbee.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca0cb16bca4a60b39020a612616b5fcc",
+    "url": "/frontend-test-mambo/static/media/img_ihponex.ca0cb16b.png"
+  },
+  {
+    "revision": "deb5a66217ceaa6106d05cd8cac43cdf",
+    "url": "/frontend-test-mambo/static/media/img_banner_res.deb5a662.png"
+  },
+  {
+    "revision": "61960d890389ccf35f6a96b80eae2b2a",
+    "url": "/frontend-test-mambo/static/media/img_banner01.61960d89.png"
+  },
+  {
+    "revision": "310fae73803f6a19be677659b3a9b127",
+    "url": "/frontend-test-mambo/static/media/img_banner.310fae73.jpg"
+  },
+  {
+    "revision": "cb66f1e557e7126d8e1d",
+    "url": "/frontend-test-mambo/static/js/runtime~main.cb66f1e5.js"
+  },
+  {
+    "revision": "5bcd6be15ae9060bb15a",
+    "url": "/frontend-test-mambo/static/js/main.5bcd6be1.chunk.js"
+  },
+  {
+    "revision": "a30620cd5ad423f1a60f",
+    "url": "/frontend-test-mambo/static/js/1.a30620cd.chunk.js"
+  },
+  {
+    "revision": "5bcd6be15ae9060bb15a",
+    "url": "/frontend-test-mambo/static/css/main.20e90788.chunk.css"
+  },
+  {
+    "revision": "b04c20f8c6c39bbb76f912e53bb2375d",
+    "url": "/frontend-test-mambo/index.html"
+  }
+];
